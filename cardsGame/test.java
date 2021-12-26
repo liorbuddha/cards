1671e98cb31dbd7adb8f11a40d6f2edf57566067
liorbuddha <51681260+liorbuddha@users.noreply.github.com>
2021-12-26 23:58:06 +0200

    Update test.java

--- cardsGame/test.java
+++ cardsGame/test.java
@@ -3,14 +3,14 @@ package cardsGame;
 public class test {
 	//Table test
 	public static void main(String[] args) {
-		int numPlayers = 5, cardsEach = 2;
+		int numPlayers = 5, cardsEach = 5;
 		Player[] players_list = new Player[numPlayers];
 		for (int i=0;i<players_list.length;i++) {
 			players_list[i]=new Player(i);
 		}
 		Table tb = new Table(players_list);
 		tb.shuffleDeck();
-		tb.play();
+		tb.play_war();
 		System.out.println(tb.cardsLeft()+" cards left in the deck");
 		
 	}
